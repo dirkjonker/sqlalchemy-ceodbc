4be4183ec32651ab3fcde56c618caf5b45aab056
Dirk Jonker <d.jonker@coolblue.nl>
2016-12-19 09:31:04 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlalchemy_ceodbc',
+    version='0.1.0',
+    description='SQLAlchemy dialect for ceODBC',
+    author='Dirk Jonker',
+    author_email='dirkjonker@gmail.com',
+    url='https://github.com/dirkjonker/sqlalchemy-ceodbc',
+    download_url='https://github.com/dirkjonker/sqlalchemy-ceodbc/tarball/0.1.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    packages=find_packages(),
+    entry_points={
+        'sqlalchemy.dialects':
+            ['mssql.ceODBC = sqlalchemy_ceodbc.dialect:MSDialect_ceODBC']
+    },
+    license='MIT',
+    install_requires=['sqlalchemy'],
+)
